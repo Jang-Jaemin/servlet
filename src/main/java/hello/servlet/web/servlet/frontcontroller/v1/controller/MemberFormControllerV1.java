972884715804caffc3f956539571c860a625c3f3
jang jae min <wkwovlf12@naver.com>
2022-08-11 18:09:42 +0900

    22.8.11

---
+++ src/main/java/hello/servlet/web/servlet/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.servlet.frontcontroller.v1.controller;
+
+import hello.servlet.web.servlet.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+
+    @Override
+    public void process(HttpServlet request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward((ServletRequest) request, response);
+    }
+}
