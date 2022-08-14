8a86f81e1d961197b790dafac4f97d74996850d1
jang jae min <wkwovlf12@naver.com>
2022-08-14 22:54:35 +0900

    22.8.14

---
+++ src/main/java/hello/servlet/web/servlet/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,11 @@
+package hello.servlet.web.servlet.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+
+
+    String process(Map<String, String>, Map<String,Object>model);
+
+}
