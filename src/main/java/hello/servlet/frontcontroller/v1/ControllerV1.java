70d207254d7320da497581ccd01d395ed542fb61
jang jae min <wkwovlf12@naver.com>
2022-08-15 22:52:45 +0900

    22.8.15

---
+++ src/main/java/hello/servlet/frontcontroller/v1/ControllerV1.java
@@ -0,0 +1,15 @@
+// 서블릿과 비슷한 모양의 컨트롤러 인터페이스를 도입한다. 각 컨트롤러들은 이 인터페이스를 구현하면된다.
+// 프론트 컨트롤러는 이 인터페이스를 호출해서 구현과 관계없이 로직의 일관성을 가져갈 수 있다.
+// 이제 이 인터페이스를 구현한 컨트롤러를 만들어보자. 지금 단계에서는 기존 로직을 최대한 유지하는게 핵심이다.
+
+package hello.servlet.frontcontroller.v1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV1 {
+    void process(HttpServlet request, HttpServletResponse response) throws ServletException, IOException;
+
+}
